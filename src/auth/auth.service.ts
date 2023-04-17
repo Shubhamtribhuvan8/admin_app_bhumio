104467e7d3a6542de779797cd144b35b72073bd5
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-04-17 07:54:25 +0530

    auth-signup-signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from "@nestjs/common";
 @Injectable({})
 export class AuthService{
+signup (){
+return {msg:"I have a signup"}
+}
 
+
+signin(){
+    return {msg:"I have a signin"}
+  }
 }
