25064a856d420c462245d9909a73ecfbf13739fb
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-04-17 11:30:53 +0530

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { BookmarkModule } from './bookmark/bookmark.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule,  MongooseModule.forRoot('mongodb://localhost/nestjs-app'),],
+  imports: [AuthModule, UserModule, BookmarkModule,  MongooseModule.forRoot('mongodb+srv://shubhamtribhuvan:1234@cluster0.dkffjqp.mongodb.net/?retryWrites=true&w=majority'),],
 })
 
 export class AppModule {}
